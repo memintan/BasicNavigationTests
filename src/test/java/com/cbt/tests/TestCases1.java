7f8bd4776fe4df7f646918ea3b8d9119b599f666
KaterynaSemenenko <ksemenenko001@gmail.com>
2019-11-18 00:45:15 -0600

    Commit Message

---
+++ src/test/java/com/cbt/tests/TestCases1.java
@@ -0,0 +1,118 @@
+package com.cbt.tests;
+
+import com.cbt.utilities.BrowserFactory;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import java.util.List;
+
+public class TestCases1 {
+    private WebDriver driver;
+
+    @BeforeMethod
+    public void setup(){
+        driver = BrowserFactory.getDriver("chrome");
+        driver.get("https://practice-cybertekschool.herokuapp.com");
+        driver.manage().window().maximize();
+    }
+
+    @Test(description = "Verify the message and home button")
+    public void testCase1 () throws InterruptedException {
+        driver.findElement(By.xpath("//a[text()='Sign Up For Mailing List']")).click();
+
+        WebElement name = driver.findElement(By.xpath("//input[@type='text']"));
+        WebElement email = driver.findElement(By.cssSelector("[type='email']"));
+        name.sendKeys("Kateryna");
+        email.sendKeys("KSemenenko@gmail.com");
+        Thread.sleep(1000);
+        driver.findElement(By.cssSelector("[class='radius'][name='wooden_spoon']")).click();
+
+        String expected = "Thank you for signing up. Click the button below to return to the home page.";
+        String actual = driver.findElement(By.xpath("//h3[@class='subheader']")).getText();//h3[@class='subheader']
+        Assert.assertEquals(expected,actual,"FAIL");
+
+        WebElement homeButton = driver.findElement(By.id("wooden_spoon"));
+        Assert.assertTrue(homeButton.isDisplayed(),"Home button is not displayed");
+
+
+
+    }
+
+    @Test (description = " Verify that number of listed examples is equals to 48 ")
+    public void testCase2() {
+        List<WebElement> list = driver.findElements(By.xpath("//*[@class='list-group-item']"));
+        int expected = 48;
+        Assert.assertEquals(list.size(),expected,"Number of listed examples is not equal to 48");
+
+    }
+
+    @Test (description = " Verify that result message is displayed: “Clicked on button one!” ")
+    public void testCase3() {
+         driver.findElement(By.linkText("Multiple Buttons")).click();
+         driver.findElement(By.cssSelector("[onclick='button1()']")).click();
+         String expected = "Clicked on button one!";
+         String actual = driver.findElement(By.id("result")).getText();
+         Assert.assertEquals(expected, actual, "Message is wrong");
+    }
+
+    @Test (description = "Verify that warning message is displayed: FIRST NAME box")
+    public void testCase4() {
+        driver.findElement(By.linkText("Registration Form")).click();
+        driver.findElement(By.xpath("//*[@type='text'][@name='firstname']")).sendKeys("123");
+        // String expected = "first name can only consist of alphabetical letters";
+        WebElement result = driver.findElement(By.xpath("//*[@class = 'help-block'][3] "));
+        //  Assert.assertEquals(expected,actual);
+        Assert.assertTrue(result.isDisplayed());
+
+    }
+
+    @Test (description = "Verify that warning message is displayed: LAST NAME box")
+    public void testCase5() {
+        driver.findElement(By.linkText("Registration Form")).click();
+        driver.findElement(By.name("lastname")).sendKeys("123");
+        WebElement result = driver.findElement(By.cssSelector("[class='help-block'][data-bv-result='INVALID']"));
+        Assert.assertTrue(result.isDisplayed());
+
+    }
+
+    @Test (description = "Verify that warning message is displayed: USERNAME box")
+    public void testCase6() {
+        driver.findElement(By.linkText("Registration Form")).click();
+        driver.findElement(By.name("username")).sendKeys("user");
+        WebElement result = driver.findElement(By.cssSelector("[data-bv-for='username'][data-bv-validator='stringLength']"));
+        Assert.assertTrue(result.isDisplayed(),"Wrong message");
+    }
+
+    @Test (description = "Verify that warning message is displayed: EMAIL box")
+    public void testCase7() {
+        driver.findElement(By.linkText("Registration Form")).click();
+        driver.findElement(By.cssSelector("[placeholder*='email']")).sendKeys("esters@email");
+        WebElement message1 = driver.findElement(By.xpath("//small[@data-bv-validator='emailAddress']"));
+        WebElement message2 = driver.findElement(By.cssSelector("[data-bv-validator='regexp'][data-bv-for='email']"));
+        Assert.assertTrue(message1.isDisplayed(), "First message is NOT displayed");
+        Assert.assertTrue(message2.isDisplayed(), "Second message is NOT displayed");
+    }
+
+    @Test (description = "Verify that warning message is displayed: PHONE box")
+    public void testCase8() {
+        driver.findElement(By.linkText("Registration Form")).click();
+        driver.findElement(By.cssSelector(".form-control[name='phone']")).sendKeys("5711234354");
+        WebElement message = driver.findElement(By.xpath("//*[contains(text(),'Phone format is not correct')] [@class='help-block']"));
+        Assert.assertTrue(message.isDisplayed(), "Message is NOT displayed");
+    }
+
+
+
+
+
+
+    @AfterMethod
+    public void teardown(){
+        driver.quit();
+    }
+}
